69f3fa664889b95c719d607a02daa54d2097dba9
vinod <vinod/reddy>
2020-02-03 23:54:23 +0530

    Started info

---
+++ me/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import about from './components/about';
+
+function App() {
+  return (
+    <div className="App">
+    <about />
+    </div>
+  );
+}
+
+export default App;
---
+++ me/src/components/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+ class about extends Component {
+    render() {
+        return (
+            <div>
+              <h1>Mr.Vinod kumar Reddy</h1> 
+              <h2>BlockChain-Developer</h2> 
+            </div>
+        )
+    }
+}
+
+export default about
